b51eacb059fc3afcc323653337b7af411b00f1a1
Zatelbeeit <breaul@gmail.com>
2019-09-30 16:50:04 +0200

    3eme fois

--- script.js
+++ script.js
@@ -1,49 +1,94 @@
-var divElement = document.getElementsByTagName ("div")[0];
+var divElement = document.getElementsByTagName("div")[0];
+var button = document.getElementsByTagName("button")[0];
+var colInput = document.getElementById("col");
+var rowInput = document.getElementById("row");
 
 var tableElement = document.createElement("table");
 //tableElement.id = "table";
-divElement.appendChild (tableElement);
+divElement.appendChild(tableElement);
 
-var nbCell = 16;
-var numbers=[]
-
-
-
-// var numbers = ["1","2","3","4","5","6","7","8", "1","2","3","4","5","6","7","8"]
-for (k=1; k <= nbCell/2; k++){
-    numbers.push(2*(k))
-}
-
-for (i = 0; i < nbCell; i++) {
-    var random_index = Math.floor(Math.random() * (i + 1));
-    var temp = numbers[i];
-    numbers[i] = numbers[random_index];
-    numbers[random_index] = temp;
-}
+var click1 = null;
+var click2 = null;
 
 
-var cpt = 0
+button.addEventListener("click", function () {
 
-for (let i=1; i < 5; i++){
-    var trElement = document.createElement("tr");
-    tableElement.appendChild(trElement)
-    
-    for (let j=1; j < 5; j++){
-        var cellElement = document.createElement("td");
-        trElement.appendChild(cellElement);
-        cellElement.innerHTML = numbers[cpt++];
-        // cellElement.classList.add("hidden");
-        // cellElement.addEventListener("click",function(){
-            //     this.classList.remove("hidden")
-            // });
-            
-        }   
-        
+    tableElement.innerHTML = ""
+
+
+    var col = colInput.value
+    var row = rowInput.value
+
+    var nbCell = col * row
+
+
+    if (nbCell % 2 == 0) {
+
+        var numbers = []
+
+
+        for (k = 1; k <= nbCell; k++) {
+            numbers.push(k);
+            numbers.push(k);
+        }
+
+        for (i = 0; i < nbCell; i++) {
+            var random_index = Math.floor(Math.random() * (i + 1));
+            var temp = numbers[i];
+            numbers[i] = numbers[random_index];
+            numbers[random_index] = temp;
+        }
+
+        var cpt = 0
+
+        for (let i = 0; i < row; i++) {
+            var trElement = document.createElement("tr");
+            tableElement.appendChild(trElement)
+
+            for (let j = 0; j < col; j++) {
+                var cellElement = document.createElement("td");
+                trElement.appendChild(cellElement);
+                cellElement.innerHTML = numbers[cpt++];
+                cellElement.classList.add("hidden");
+                cellElement.addEventListener("click", function () {
+                    this.classList.remove("hidden");
+
+                    if(click1 == null){
+                        click1 = this;
+                    } else{
+                        click2 = this;
+                        if(click1.innerHTML != click2.innerHTML){
+                            setTimeout(function(){
+                                click1.classList.add("hidden");
+                                click2.classList.add("hidden");
+
+                                click1 = null
+                                click2 = null
+                            }, 1000)
+                        } else {
+                            click1 = null
+                            click2 = null
+                        }
+
+                    }
+
+                });
+
+            }
+
+        }
+
+    } else {
+        colInput.style.border = "solid 1px red";
+        alert("change le chiffre imbécile!!");
     }
-    
-    
-    
-    
+})
+
+
+
+
+
+
 
 
 
